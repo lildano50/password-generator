c919a2cc3ab3705d733f977806236bf6dd303c78
Daniel Park <danielapark87@gmail.com>
2023-08-19 11:19:24 -0500

    updated js and html

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
+// GIVEN I need a new, secure password
+
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+    //passwordLength = prompt("How long would you like the password to be? \n Password must be between 8 and 128 characters.")
+    //randomPassword = specialChar[Math.floor(Math.random() * passwordLength)]
+
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
+
+
+
 // Assignment code here
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+let specialChar = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numbers = "0123456789";
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword(length, characters) {
+    var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
